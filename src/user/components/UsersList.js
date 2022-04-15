803bf8d72f0456bb1ac5e39ec7c7499ad1af727d
Jianing Li <98203486+Jackyy-Li@users.noreply.github.com>
2022-04-14 22:40:34 -0700

    Implement UsersPlaces

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import UserItem from './UserItem';
-import Card from '../../shared/components/UIElement/Card';
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
-  if (props.items.length === 0) { // Logic to decide what to present 
+  if (props.items.length === 0) {
     return (
       <div className="center">
         <Card>
-        <h2>No users found.</h2>
+          <h2>No users found.</h2>
         </Card>
       </div>
     );
